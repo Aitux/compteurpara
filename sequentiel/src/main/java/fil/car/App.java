4c5c4d682f4d553fc864f59b845fb227feabdbc9
Simon Vandeputte <simon.vandeputte@univ-lille.fr>
2019-01-29 14:37:38 +0100

    test seq

--- sequentiel/src/main/java/fil/car/App.java
+++ sequentiel/src/main/java/fil/car/App.java
@@ -1,7 +1,13 @@
 package fil.car;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Hello world!
@@ -17,8 +23,25 @@ public class App {
             System.err.println("Argument needed: absolute_path_to_file");
             System.exit(-1);
         }
+        File f = new File(args[0]);
+        try {
+            BufferedReader bdr = new BufferedReader(new FileReader(f));
+            List<String> str = bdr.lines().collect(Collectors.toList());
+            StringBuilder tmp = new StringBuilder();
+            for (String s :
+                    str) {
+                tmp.append(s).append("\n");
+            }
+            String[] result = tmp.toString().split("\\s");
+            App a = new App();
+            System.out.println(a.maxOccu(result) + " is present in the most occurence(s)");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String maxOccu(String[] result){
         Map<String, Integer> count = new HashMap<>();
-        String[] result = args[0].split("\\s");
         for (int i = 0; i < result.length; i++) {
             if (count.containsKey(result[i])) {
                 int val = count.get(result[i]);
@@ -27,14 +50,14 @@ public class App {
                 count.put(result[i], 1);
             }
         }
-        Map.Entry<String, Integer> maxEntry = null;
 
+        Map.Entry<String, Integer> maxEntry = null;
         for (Map.Entry<String, Integer> entry : count.entrySet()) {
             if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) > 0) {
                 maxEntry = entry;
             }
         }
-        String maxi = maxEntry.getKey();
-        System.out.println(maxi + " is present in " + maxEntry.getValue() + " occurence(s)");
+
+        return maxEntry.getKey();
     }
 }
--- sequentiel/src/test/java/fil/car/AppTest.java
+++ sequentiel/src/test/java/fil/car/AppTest.java
@@ -1,20 +1,19 @@
 package fil.car;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void maxOccuTest() {
+        App a = new App();
+        String test = "Je pense que le nombre d'occurence d'un mot dans une phrase devrait être le mot le plus impressionant dans cette phrase pour les bienfaits du test ! C'est pourquoi j'ai jeté mon dévolu sur le le le le le le le ";
+        String[] result = test.split("\\s");
+
+        Assert.assertEquals("le", a.maxOccu(result));
     }
 }
